018ad6f96b50bd21f50733888dd4b1225186c4d9
conght11 <conght11>
2022-02-15 18:36:10 +0700

    change pom to ezy

--- src/main/java/com/ezy/databasesetting/controller/PingController.java
+++ src/main/java/com/ezy/databasesetting/controller/PingController.java
@@ -1,6 +1,5 @@
 package com.ezy.databasesetting.controller;
 
-import com.ezy.databasesetting.repository.DatabaseListRepository;
 import com.tvd12.ezyhttp.server.core.annotation.Controller;
 import com.tvd12.ezyhttp.server.core.annotation.DoGet;
 import lombok.AllArgsConstructor;
@@ -10,11 +9,9 @@ import lombok.AllArgsConstructor;
 @Controller("/api/v1")
 public class PingController {
 
-    private final DatabaseListRepository databaseListRepository;
 
     @DoGet("/ping")
     public String ping() {
-//        databaseListRepository.findAll();
         return "OK!";
     }
 }
